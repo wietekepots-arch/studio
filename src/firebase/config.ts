fb890380fc3eaf050a63bfb54e4542aaf609c4ab
Wieteke Pots <wieteke.pots@greenberry.nl>
2026-03-03 16:11:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6608207418-ad56b",
+  "appId": "1:179305077204:web:80dfde152ab2cad1a41e41",
+  "apiKey": "AIzaSyBQFB5PLIbppjmlmdTjIRKc9aAVeGqd0vo",
+  "authDomain": "studio-6608207418-ad56b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "179305077204"
+};
